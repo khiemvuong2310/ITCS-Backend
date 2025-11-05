635c1ee494d1e0a4d115030338af3b8f4e4aed6b
Nguyễn Đình Bin <binnd26052002@gmail.com>
2025-11-05 13:57:03 +0700

    update import export service

---
+++ FA25-CP.CryoFert/FSCMS.Service/ReponseModel/CryoExportResponseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FSCMS.Service.ReponseModel
+{    public class CryoExportResponse
+    {
+        public Guid Id { get; set; }
+        public Guid LabSampleId { get; set; }
+        public Guid CryoLocationId { get; set; }
+        public DateTime ExportDate { get; set; }
+        public Guid? ExportedBy { get; set; }
+        public Guid? WitnessedBy { get; set; }
+        public string? Reason { get; set; }
+        public string? Destination { get; set; }
+        public string? Notes { get; set; }
+        public bool IsThawed { get; set; }
+        public DateTime? ThawingDate { get; set; }
+        public string? ThawingResult { get; set; }
+        public LabSampleResponse? LabSample { get; set; }
+        public CryoLocationResponse? CryoLocation { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
